af3c03a3bd1a9c40f7ae55652c3eb740f82e7663
Luca Della Vedova <lucadv@intrinsic.ai>
2024-09-20 18:51:12 +0800

    Cleanup
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>

--- nav2_minimal_tb4_sim/launch/spawn_tb4.launch.py
+++ nav2_minimal_tb4_sim/launch/spawn_tb4.launch.py
@@ -14,14 +14,11 @@
 # limitations under the License.
 
 import os
-from pathlib import Path
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import AppendEnvironmentVariable
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node, PushRosNamespace
