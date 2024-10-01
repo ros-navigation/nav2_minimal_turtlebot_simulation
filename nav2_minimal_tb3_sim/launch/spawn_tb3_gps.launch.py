f0eeedbc95d9f7cc8a513f7d46a84b3d08a3d395
Stevedan Ogochukwu Omodolor <61468301+stevedanomodolor@users.noreply.github.com>
2024-10-02 01:47:07 +0200

    Feat/add gps launch (#14)
    * include gps launch
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * working version
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * lintering
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * Update nav2_minimal_tb3_sim/configs/turtlebot3_waffle_gps_bridge.yaml
    
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>
    Signed-off-by: Stevedan Ogochukwu Omodolor <61468301+stevedanomodolor@users.noreply.github.com>
    
    * Change naming
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    * change artifat from v1 to v4
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    
    ---------
    
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>
    Signed-off-by: Stevedan Ogochukwu Omodolor <61468301+stevedanomodolor@users.noreply.github.com>
    Co-authored-by: Steve Macenski <stevenmacenski@gmail.com>

---
+++ nav2_minimal_tb3_sim/launch/spawn_tb3_gps.launch.py
@@ -0,0 +1,132 @@
+# Copyright (C) 2023 Open Source Robotics Foundation
+# Copyright (C) 2024 Open Navigation LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from pathlib import Path
+
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import AppendEnvironmentVariable
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions.command import Command
+from launch.substitutions.find_executable import FindExecutable
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    bringup_dir = get_package_share_directory('nav2_minimal_tb3_sim')
+
+    namespace = LaunchConfiguration('namespace')
+    robot_name = LaunchConfiguration('robot_name')
+    robot_sdf = LaunchConfiguration('robot_sdf')
+    pose = {
+        'x': LaunchConfiguration('x_pose', default='-2.00'),
+        'y': LaunchConfiguration('y_pose', default='-0.50'),
+        'z': LaunchConfiguration('z_pose', default='0.01'),
+        'R': LaunchConfiguration('roll', default='0.00'),
+        'P': LaunchConfiguration('pitch', default='0.00'),
+        'Y': LaunchConfiguration('yaw', default='0.00'),
+    }
+
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace', default_value='', description='Top-level namespace'
+    )
+
+    declare_robot_name_cmd = DeclareLaunchArgument(
+        'robot_name',
+        default_value='turtlebot3_waffle_gps',
+        description='name of the robot',
+    )
+
+    declare_robot_sdf_cmd = DeclareLaunchArgument(
+        'robot_sdf',
+        default_value=os.path.join(bringup_dir, 'urdf', 'gz_waffle_gps.sdf.xacro'),
+        description='Full path to robot sdf file to spawn the robot in gazebo',
+    )
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        namespace=namespace,
+        parameters=[
+            {
+                'config_file': os.path.join(
+                    bringup_dir, 'configs', 'turtlebot3_waffle_gps_bridge.yaml'
+                ),
+                'expand_gz_topic_names': True,
+                'use_sim_time': True,
+            }
+        ],
+        output='screen',
+    )
+
+    spawn_model = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        namespace=namespace,
+        arguments=[
+            '-name',
+            robot_name,
+            '-string',
+            Command(
+                [
+                    FindExecutable(name='xacro'),
+                    ' ',
+                    'namespace:=',
+                    LaunchConfiguration('namespace'),
+                    ' ',
+                    robot_sdf,
+                ]
+            ),
+            '-x',
+            pose['x'],
+            '-y',
+            pose['y'],
+            '-z',
+            pose['z'],
+            '-R',
+            pose['R'],
+            '-P',
+            pose['P'],
+            '-Y',
+            pose['Y'],
+        ],
+    )
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH', os.path.join(bringup_dir, 'models')
+    )
+    set_env_vars_resources2 = AppendEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH', str(Path(os.path.join(bringup_dir)).parent.resolve())
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_robot_name_cmd)
+    ld.add_action(declare_robot_sdf_cmd)
+
+    ld.add_action(set_env_vars_resources)
+    ld.add_action(set_env_vars_resources2)
+
+    ld.add_action(bridge)
+    ld.add_action(spawn_model)
+    return ld
